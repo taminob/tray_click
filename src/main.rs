e1c5f5646af3ba180d95f98b344019c5d1b342f2
Tamino Bauknecht <dev@tb6.eu>
2021-11-21 00:35:15 +0100

    clap for argument parsing added; name and icon application parameters added

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use clap::{App, Arg};
 use tray_item::TrayItem;
 mod entries;
 mod entry;
 
-const APPLICATION_NAME: &str = "tray_click";
-const APPLICATION_ICON: &str = "arrow-down-double";
+const DEFAULT_APPLICATION_NAME: &str = "tray_click";
+const DEFAULT_APPLICATION_ICON: &str = "arrow-down-double";
 
 fn add_item_to_tray<EntryType: 'static>(tray: &mut TrayItem)
 where
@@ -14,12 +15,37 @@ where
 }
 
 fn main() {
+    let args = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(
+            Arg::with_name("name")
+                .help("Set the display name for the application")
+                .short("n")
+                .long("name")
+                .takes_value(true)
+                .value_name("STRING")
+                .default_value(DEFAULT_APPLICATION_NAME),
+        )
+        .arg(
+            Arg::with_name("icon")
+                .help("Set the display icon for the application")
+                .short("i")
+                .long("icon")
+                .takes_value(true)
+                .value_name("STRING")
+                .default_value(DEFAULT_APPLICATION_ICON),
+        )
+        .get_matches();
+
     gtk::init().expect("failed to init gtk");
+    let app_name = args.value_of("name").unwrap();
+    glib::set_application_name(app_name);
 
     let mut tray =
-        TrayItem::new(APPLICATION_NAME, APPLICATION_ICON).expect("failed to create tray");
-    tray.add_label(APPLICATION_NAME)
-        .expect("failed to add tray label");
+        TrayItem::new(app_name, args.value_of("icon").unwrap()).expect("failed to create tray");
+    tray.add_label(app_name).expect("failed to add tray label");
     add_item_to_tray::<entries::EchoEntry>(&mut tray);
     add_item_to_tray::<entries::NotificationTestEntry>(&mut tray);
     add_item_to_tray::<entries::NotificationEnableEntry>(&mut tray);
